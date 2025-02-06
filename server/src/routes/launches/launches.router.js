924571ceed5183c08b7983290531d6c737b0a2b5
NightsHigh <soren913@gmail.com>
2025-02-06 08:32:02 +0100

    Added POST AND DELETE function to the backend aswell as the frontend delete button on the object

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -2,11 +2,12 @@ const express = require('express')
 const {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 } = require('./launches.controllers')
 
 const launchesRouter = express.Router()
 
 launchesRouter.get('/', httpGetAllLaunches)
 launchesRouter.post('/', httpAddNewLaunch)
-
+launchesRouter.delete('/:id', httpAbortLaunch)
 module.exports = launchesRouter
